f8292de9db41bafe9e0ba06cc0f2711df1174259
cesarbrancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-02-24 19:59:44 -0300

    Fixed main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CheckoutController } from './checkout/checkout.controller';
 import { CheckoutsModule } from './checkouts/checkouts.module';
 
 /* Decorator */
 @Module({ 
   imports: [CheckoutsModule],
-  controllers: [AppController, CheckoutController], /* MVC structure */
+  controllers: [AppController], /* MVC structure */
   providers: [AppService], 
 })
 export class AppModule {} /* Root module */
