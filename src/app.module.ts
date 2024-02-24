77a26789343fdea673ff35192093165dca8ea6bc
cesarbrancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-02-24 19:56:38 -0300

    Added checkouts resource]

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CheckoutController } from './checkout/checkout.controller';
+import { CheckoutsModule } from './checkouts/checkouts.module';
 
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+/* Decorator */
+@Module({ 
+  imports: [CheckoutsModule],
+  controllers: [AppController, CheckoutController], /* MVC structure */
+  providers: [AppService], 
 })
-export class AppModule {}
+export class AppModule {} /* Root module */
