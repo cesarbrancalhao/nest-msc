3b704c1c2b608d5d6ca92b465b8944947e8441a0
cesarbrancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-02-29 06:17:02 -0300

    Created a placeholder items list

---
+++ constants/products-list.ts
@@ -0,0 +1,28 @@
+const PRODUCTS_LIST = [
+    {
+      "id": 1,
+      "name": 'Product 1',
+      "description": 'Product 1 description',
+      "image_url": 'https://example.com/image1.png',
+      "price": 300,
+      "product_id": 1,
+    },
+    {
+      "id": 2,
+      "name": 'Product 2',
+      "description": 'Product 2 description',
+      "image_url": 'https://example.com/image2.png',
+      "price": 400,
+      "product_id": 2,
+    },
+    {
+      "id": 3,
+      "name": 'Product 3',
+      "description": 'Product 3 description',
+      "image_url": 'https://example.com/image3.png',
+      "price": 500,
+      "product_id": 3,
+    }
+  ];
+
+  export default PRODUCTS_LIST;
