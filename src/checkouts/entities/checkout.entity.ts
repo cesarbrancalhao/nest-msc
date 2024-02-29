55ed98c97e5dc479e03337566af019c910c24068
cesarbrancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-02-28 23:37:02 -0300

    Added checkoutCommand entity and fixed checkoutProduct types

--- src/checkouts/entities/checkout.entity.ts
+++ src/checkouts/entities/checkout.entity.ts
@@ -1,4 +1,28 @@
-import { Column, CreateDateColumn, Entity, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { 
+    Column, 
+    CreateDateColumn, 
+    Entity, 
+    ManyToOne, 
+    OneToMany, 
+    PrimaryGeneratedColumn
+} from "typeorm";
+
+export type CreateCheckoutCommand = {
+
+    items: {
+
+        quantity: number;
+        price: number;
+        product: {
+            name: string;
+            description: string;
+            image_url: string;
+            product_id: number;
+        };
+
+    }[];
+
+};
 
 export enum CheckoutStatus {
     PENDING = 'PENDING',
@@ -21,6 +45,28 @@ export class Checkout {
     @CreateDateColumn()
     created_at: Date;
 
+    @OneToMany(() => CheckoutItem, (item) => item.checkout)
+    items: CheckoutItem[];
+
+    static create(input: CreateCheckoutCommand) {
+        
+        const checkout = new Checkout();
+        checkout.items = input.items.map(item => {
+
+            const checkoutItem = new CheckoutItem();
+            checkoutItem.quantity = item.quantity;
+            checkoutItem.price = item.price;
+            checkoutItem.product = new CheckoutProduct();
+            checkoutItem.product.name = item.product.name;
+            checkoutItem.product.description = item.product.description;
+            checkoutItem.product.image_url = item.product.image_url;
+            checkoutItem.product.product_id = item.product.product_id;
+            return checkoutItem;
+
+        });
+
+    }
+
 }
 
 @Entity()
@@ -36,7 +82,7 @@ export class CheckoutProduct {
     description: string;
 
     @Column()
-    image_url: number;
+    image_url: string;
 
     @Column()
     product_id: number; /* Product ID from another microservice */
