d714e5ea62880b3f220722d3b136d1b2e9955d47
cesarbrancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-02-29 17:27:48 -0300

    Added class-validator lib, updated checkoutDto

--- src/checkouts/dto/create-checkout.dto.ts
+++ src/checkouts/dto/create-checkout.dto.ts
@@ -1,7 +1,25 @@
+import { ArrayNotEmpty, IsArray, IsInt, IsNotEmpty, IsPositive, ValidateNested } from "class-validator";
+
 export class CheckoutItemDto {
+
+    @IsInt()
+    @IsPositive()
+    @IsNotEmpty()
     quantity: number;
+
+    @IsInt()
+    @IsPositive()
+    @IsNotEmpty()
     product_id: number;
+
 };
+
 export class CreateCheckoutDto {
+
+    @IsNotEmpty()
+    @IsArray()
+    @ArrayNotEmpty()
+    @ValidateNested({ each: true })
     items: CheckoutItemDto[];
+    
 }
