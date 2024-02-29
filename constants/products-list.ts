099b780b2d4806874a7bf60b946aae4cf9c3fd5d
cesarbrancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-02-29 06:23:20 -0300

    Updated placeholding products list

--- constants/products-list.ts
+++ constants/products-list.ts
@@ -5,7 +5,7 @@ const PRODUCTS_LIST = [
       "description": 'Product 1 description',
       "image_url": 'https://example.com/image1.png',
       "price": 300,
-      "product_id": 1,
+      "category_id": 1,
     },
     {
       "id": 2,
@@ -13,7 +13,7 @@ const PRODUCTS_LIST = [
       "description": 'Product 2 description',
       "image_url": 'https://example.com/image2.png',
       "price": 400,
-      "product_id": 2,
+      "category_id": 2,
     },
     {
       "id": 3,
@@ -21,7 +21,7 @@ const PRODUCTS_LIST = [
       "description": 'Product 3 description',
       "image_url": 'https://example.com/image3.png',
       "price": 500,
-      "product_id": 3,
+      "category_id": 3,
     }
   ];
 
