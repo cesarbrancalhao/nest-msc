a6ff9c2b776fe829ac3a67a53f68522150d7bbc5
cesarbrancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-03-01 13:02:07 -0300

    Updated to Prisma version

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,36 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CheckoutsModule } from './checkouts/checkouts.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Checkout } from './checkouts/entities/checkout.entity';
-import { CheckoutItem } from './checkouts/entities/checkout-item.entity';
-import { CheckoutProduct } from './checkouts/entities/checkout-product.entity';
 
-/* Decorator */
-@Module({ 
-  
-	imports: [
-
-		TypeOrmModule.forRoot({
-
-		type: 'mysql',
-		host: 'localhost',
-		port: 3306,
-		username: 'root',
-		password: 'root',
-		database: 'nest',
-		entities: [Checkout, CheckoutItem, CheckoutProduct],
-		synchronize: true,
-		logging: true,
-
-		}),
-		CheckoutsModule
-
-	],
-	controllers: [AppController], /* MVC structure */
-	providers: [AppService], 
-
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
-
-export class AppModule {} /* Root module */
+export class AppModule {}
--- src/checkouts/dto/update-checkout.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCheckoutDto } from './create-checkout.dto';
-
-export class UpdateCheckoutDto extends PartialType(CreateCheckoutDto) {}
